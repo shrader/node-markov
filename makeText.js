3313adad15f6753a5aeee85fa3932a672a3a76a9
John <figurefactory@gmail.com>
2021-03-24 01:27:03 -0400

    completed markov chains, need to add testing

--- makeText.js
+++ makeText.js
@@ -1,2 +1,41 @@
 /** Command-line tool to generate Markov text. */
 
+const fsP = require("fs/promises");
+const axios = require("axios");
+const mm = require('./markov.js');
+
+/** read file at path and print it out. */
+
+async function cat(path) {
+  let content;
+  let chains;
+  try {
+    content = await fsP.readFile(path, "utf8");
+    chains = new mm(content);
+    console.log(chains.getText());
+  } catch (err) {
+    console.error(`Error reading ${path}: ${err}`);
+    process.exit(1);
+  }
+}
+
+/** read page at URL and print it out. */
+
+async function webCat(url) {
+  let resp;
+  try {
+    resp = await axios({ url });
+    chains = new mm(resp.data);
+} catch (err) {
+    console.error(`Error fetching ${url}: ${err}`);
+    process.exit(1);
+}
+console.log(chains.getText());
+
+}
+
+let path = process.argv[2];
+
+let resultPromise = path.startsWith("http")
+    ? webCat(path)
+    : cat(path);
--- markov.js
+++ markov.js
@@ -1,13 +1,34 @@
 /** Textual markov chain generator */
 
+/* 
+You should be able to instantiate it like this:
 
-class MarkovMachine {
+let mm = new MarkovMachine("the cat in the hat");
+
+Then, whenever you want to get generated text from it:
+
+mm.makeText();
+
+mm.getText(numWords=50);
+
+*/
+
+//read in text
+//split text into word object "chains"
+//randomly select start word
+//use start word as key in obj to find next word
+//repeat until you hit null
+//return sentence(s)
+
+
+module.exports = class MarkovMachine {
 
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    // MORE CODE HERE
+    // turn words into object
+    this.makeChains(words);
   }
 
   /** set markov chains:
@@ -15,14 +36,47 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
-    // MORE CODE HERE
+  makeChains(wordsArr) {
+    let chain = {}
+    for (let index in wordsArr) {
+      if (chain[wordsArr[index]]){
+        chain[wordsArr[index]].push(wordsArr[parseInt(index) + 1] || null);
+      } else {
+        chain[wordsArr[index]] = [];
+        chain[wordsArr[index]].push(wordsArr[parseInt(index) + 1] || null);
+      }
+    }
+    this.chain = chain;
   }
 
-
+  chooseRandom(arr) {
+   let word = arr[Math.floor(Math.random() * arr.length)]
+   return word;
+  }
+  
+  
   /** return random text from chains */
-
+  
   getText(numWords = 100) {
-    // MORE CODE HERE
+    //get obj keys arr
+    let starterWords = Object.keys(this.chain);
+    let start = this.chooseRandom(starterWords);
+    let wordList = [];
+    wordList.push(start);
+    while (wordList.length < numWords) {
+      let nextWord = this.chooseRandom(this.chain[start])
+      if (nextWord === null) {
+          console.log('break');
+          break;
+      } else {
+        wordList.push(nextWord);
+        start = nextWord;
+      }
+    }
+
+    return wordList.join(' ');
+
   }
-}
+
+}  
+
